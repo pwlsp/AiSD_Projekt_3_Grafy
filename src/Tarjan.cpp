75a9067f50d096c8d7419a2a2a4c836c4549ad8d
pawluk_sa <spychala.paw@gmail.com>
2024-05-07 23:35:43 +0200

    robię commita bo mam ptsd (tylko dodałem plik z komendami i począteczek Tarjana

--- src/Tarjan.cpp
+++ src/Tarjan.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
 #include <list>
+#include <stack>
 
 #include "../include/struct_graph.h"
 #include "../include/struct_edgeList.h"
 
-void Tarjan_matrix(int **matrix, int vertices){
+void Tarjan_go_matrix(int **matrix_copy, int vertices, int *color, std::stack<int> S)
+{
+    for (int i = 0; i < vertices; i++)
+    {
+        std::cout << color[i] << " ";
+    }
 
+    std::cout << "\n";
 }
 
-void Tarjan_list(graph *L, int vertices){
+void Tarjan_matrix(int **matrix, int vertices)
+{
+    int *color;
+    color = new int[vertices];
+    for (int i = 0; i < vertices; i++)
+        color[i] = 0;
 
+    int **matrix_copy; matrix_copy = new int *[vertices];
+    for(int i = 0; i < vertices; i++) matrix_copy[i] = new int[vertices];
+    for(int i = 0; i < vertices; i++){
+        for(int j = 0; j < vertices; j++){
+            matrix_copy[i][j] = matrix[i][j];
+        }
+    }
+    for(int i = 0; i < vertices; i++){
+        for(int j = 0; j < vertices; j++){
+            std::cout << matrix_copy[i][j] << " ";
+        }
+        std::cout<<"\n";
+    }
+
+    // Tarjan_go_matrix(matrix_copy, vertices)
 }
 
-void Tarjan_table(edgeList *eList, int edges, int vertices){
-    
+void Tarjan_list(graph *L, int vertices)
+{
+}
+
+void Tarjan_table(edgeList *eList, int edges, int vertices)
+{
 }

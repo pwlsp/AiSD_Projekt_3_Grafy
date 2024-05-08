6e12ebe522d487a83561de0ae3ee8a43cfba1cc1
pawluk_sa <spychala.paw@gmail.com>
2024-05-08 14:57:09 +0200

    w dfs.cpp tylko sformatowałem, Tarjan niedokończony, od go_list zacznij

--- src/Tarjan.cpp
+++ src/Tarjan.cpp
@@ -19,9 +19,17 @@ void Tarjan_go_matrix(int **matrix, int vertices, int *color, std::stack<int> &S
     Stack.push(Vertex);
 }
 
-void Tarjan_go_list(int **matrix, int vertices, int *color, std::stack<int> &Stack, int Vertex, int *ins)
+void Tarjan_go_list(graph *L, int vertices, int *color, std::stack<int> &Stack, int Vertex, int *ins)
 {
     color[Vertex] = 1;
+    
+    for (std::list<int>::iterator it = L[Vertex].next.begin(); it != L[Vertex].next.end(); it++)
+    {
+        if ((!color[*it]) && color[Vertex] == 1)
+        {
+            Tarjan_go_matrix(L, vertices, color, Stack, *it, ins);
+        }
+    }
     for (int i = 0; i < vertices; i++)
     {
         if (matrix[Vertex][i] == 1 && (!color[i]) && color[Vertex] == 1)
@@ -33,7 +41,7 @@ void Tarjan_go_list(int **matrix, int vertices, int *color, std::stack<int> &Sta
     Stack.push(Vertex);
 }
 
-void Tarjan_go_table(int **matrix, int vertices, int *color, std::stack<int> &Stack, int Vertex, int *ins)
+void Tarjan_go_table(edgeList *eList, int vertices, int *color, std::stack<int> &Stack, int Vertex, int *ins)
 {
     color[Vertex] = 1;
     for (int i = 0; i < vertices; i++)
@@ -82,21 +90,10 @@ void Tarjan_matrix(int **matrix, int vertices)
         if (ins[i] == 0 && !color[i])
         {
             Tarjan_go_matrix(matrix, vertices, color, Stack, i, ins);
-        }   
+        }
     }
 
-    /* TO NIE JEST POTRZEBNE CHYBA
-            // POTEM OD ZALEŻNYCH
-            for (int i = 0; i < vertices; i++)
-            {
-                if (!color[i])
-                {
-                    Tarjan_go_matrix(matrix, vertices, color, Stack, i, ins);
-                }
-            }
-    */
-
-   int czarnych = 0;
+    int czarnych = 0;
     for (int j = 0; j < vertices; j++)
     {
         if (color[j] == 2)
@@ -113,15 +110,120 @@ void Tarjan_matrix(int **matrix, int vertices)
             Stack.pop();
         }
         std::cout << "\n";
-    }    
+    }
 }
 
 void Tarjan_list(graph *L, int vertices)
 {
+    // DEKLARACJE:
+    std::stack<int> Stack;
+    int ins[vertices], pierwszy = 0;
+    int *color;
+    color = new int[vertices];
+
+    // NAPEŁNIAMY ZERAMI TABLICĘ "COLOR" I "INS" (JAKI STOPIEŃ WEJŚCIOWY)
+    for (int i = 0; i < vertices; i++)
+    {
+        color[i] = 0;
+        ins[i] = 0;
+    }
+
+    // SPRAWDZAMY JAKI JEST STOPIEŃ WEJŚCIOWY WIERZCHOŁKÓW I ZAPISUJEMY W "INS"
+    for (int i = 0; i < vertices; i++)
+    {
+        for (std::list<int>::iterator it = L[i].next.begin(); it != L[i].next.end(); it++)
+        {
+            ins[*it]++;
+        }
+    }
+
+    // ###### WŁAŚCIWY ALGORYTM #######
+
+    // ROBIMY DFS'A ZACZYNAJĄC OD WIERZCHOŁKÓW NIEZALEŻNYCH
+    for (int i = 0; i < vertices; i++)
+    {
+        if (ins[i] == 0 && !color[i])
+        {
+            Tarjan_go_list(L, vertices, color, Stack, i, ins);
+        }
+    }
+
+    int czarnych = 0;
+    for (int j = 0; j < vertices; j++)
+    {
+        if (color[j] == 2)
+        {
+            czarnych++;
+        }
+    }
+
+    if (czarnych == vertices)
+    {
+        while (!Stack.empty())
+        {
+            std::cout << Stack.top() << " ";
+            Stack.pop();
+        }
+        std::cout << "\n";
+    }
 }
 
 void Tarjan_table(edgeList *eList, int edges, int vertices)
 {
+    // DEKLARACJE:
+    std::stack<int> Stack;
+    int ins[vertices], pierwszy = 0;
+    int *color;
+    color = new int[vertices];
+
+    // NAPEŁNIAMY ZERAMI TABLICĘ "COLOR" I "INS" (JAKI STOPIEŃ WEJŚCIOWY)
+    for (int i = 0; i < vertices; i++)
+    {
+        color[i] = 0;
+        ins[i] = 0;
+    }
+
+    // SPRAWDZAMY JAKI JEST STOPIEŃ WEJŚCIOWY WIERZCHOŁKÓW I ZAPISUJEMY W "INS"
+    for (int i = 0; i < vertices; i++)
+    {
+        for (int j = 0; j < vertices; j++)
+        {
+            if (matrix[i][j] == 1)
+            {
+                ins[j]++;
+            }
+        }
+    }
+
+    // ###### WŁAŚCIWY ALGORYTM #######
+
+    // ROBIMY DFS'A ZACZYNAJĄC OD WIERZCHOŁKÓW NIEZALEŻNYCH
+    for (int i = 0; i < vertices; i++)
+    {
+        if (ins[i] == 0 && !color[i])
+        {
+            Tarjan_go_matrix(matrix, vertices, color, Stack, i, ins);
+        }
+    }
+
+    int czarnych = 0;
+    for (int j = 0; j < vertices; j++)
+    {
+        if (color[j] == 2)
+        {
+            czarnych++;
+        }
+    }
+
+    if (czarnych == vertices)
+    {
+        while (!Stack.empty())
+        {
+            std::cout << Stack.top() << " ";
+            Stack.pop();
+        }
+        std::cout << "\n";
+    }
 }
 
 // ########## WYPISYWANIE MATRYCY ###########
@@ -139,3 +241,14 @@ void Tarjan_table(edgeList *eList, int edges, int vertices)
 // }
 
 // std::cout << "\n";
+
+/* TO NIE JEST POTRZEBNE CHYBA
+            // POTEM OD ZALEŻNYCH
+            for (int i = 0; i < vertices; i++)
+            {
+                if (!color[i])
+                {
+                    Tarjan_go_matrix(matrix, vertices, color, Stack, i, ins);
+                }
+            }
+    */
--- src/dfs.cpp
+++ src/dfs.cpp
@@ -6,37 +6,45 @@
 #include "../include/struct_graph.h"
 #include "../include/struct_edgeList.h"
 
-void dfs_go_matrix(int **matrix, int V, int N, bool *visited){
+void dfs_go_matrix(int **matrix, int V, int N, bool *visited)
+{
     std::cout << V << " ";
 
     visited[V] = true;
 
-    for (int i = 0; i < N; i++) {
-        if (matrix[V][i] == 1 && (!visited[i])) {
+    for (int i = 0; i < N; i++)
+    {
+        if (matrix[V][i] == 1 && (!visited[i]))
+        {
             dfs_go_matrix(matrix, i, N, visited);
         }
     }
 }
 
-void dfs_go_list(graph *L, int V, int N, bool *visited){
+void dfs_go_list(graph *L, int V, int N, bool *visited)
+{
     std::cout << V << " ";
 
     visited[V] = true;
 
-    for (std::list<int>::iterator it = L[V].next.begin(); it != L[V].next.end(); it++){
-            if(!visited[*it]){
-                dfs_go_list(L, *it, N, visited);
-            }
+    for (std::list<int>::iterator it = L[V].next.begin(); it != L[V].next.end(); it++)
+    {
+        if (!visited[*it])
+        {
+            dfs_go_list(L, *it, N, visited);
         }
+    }
 }
 
-void dfs_go_table(edgeList *eList, int V, int E, bool *visited){
+void dfs_go_table(edgeList *eList, int V, int E, bool *visited)
+{
     std::cout << V << " ";
 
     visited[V] = true;
 
-    for (int i = 0; i < E; i++) {
-        if(eList[i].out == V && !visited[eList[i].in])
+    for (int i = 0; i < E; i++)
+    {
+        if (eList[i].out == V && !visited[eList[i].in])
         {
             visited[eList[i].in] = 1;
             dfs_go_table(eList, eList[i].in, E, visited);
@@ -44,29 +52,44 @@ void dfs_go_table(edgeList *eList, int V, int E, bool *visited){
     }
 }
 
-void dfs_matrix(int **matrix, int vertices){
-    bool *visited; visited = new bool[vertices];
-    for(int i = 0; i < vertices; i++) visited[i] = 0;
-    for(int i = 0; i < vertices; i++){
-        if(!visited[i]) dfs_go_matrix(matrix, i, vertices, visited);
+void dfs_matrix(int **matrix, int vertices)
+{
+    bool *visited;
+    visited = new bool[vertices];
+    for (int i = 0; i < vertices; i++)
+        visited[i] = 0;
+    for (int i = 0; i < vertices; i++)
+    {
+        if (!visited[i])
+            dfs_go_matrix(matrix, i, vertices, visited);
     }
     std::cout << "\n";
 }
 
-void dfs_list(graph *L, int vertices){
-    bool *visited; visited = new bool[vertices];
-    for(int i = 0; i < vertices; i++) visited[i] = 0;
-    for(int i = 0; i < vertices; i++){
-        if(!visited[i]) dfs_go_list(L, i, vertices, visited);
+void dfs_list(graph *L, int vertices)
+{
+    bool *visited;
+    visited = new bool[vertices];
+    for (int i = 0; i < vertices; i++)
+        visited[i] = 0;
+    for (int i = 0; i < vertices; i++)
+    {
+        if (!visited[i])
+            dfs_go_list(L, i, vertices, visited);
     }
     std::cout << "\n";
 }
 
-void dfs_table(edgeList *eList, int edges, int vertices){
-    bool *visited; visited = new bool[vertices];
-    for(int i = 0; i < vertices; i++) visited[i] = 0;
-    for(int i = 0; i < vertices; i++){
-        if(!visited[i]) dfs_go_table(eList, i, edges, visited);
+void dfs_table(edgeList *eList, int edges, int vertices)
+{
+    bool *visited;
+    visited = new bool[vertices];
+    for (int i = 0; i < vertices; i++)
+        visited[i] = 0;
+    for (int i = 0; i < vertices; i++)
+    {
+        if (!visited[i])
+            dfs_go_table(eList, i, edges, visited);
     }
     std::cout << "\n";
 }
